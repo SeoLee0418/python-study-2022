00aa05a0dbbcbdb9a1e3ea9179bb7aa685bdff8e
SeoLee0418 <sksdltjdyd@snu.ac.kr>
2022-06-23 17:37:29 +0900

    Basic Study Sequence

---
+++ basic-python-sequence.py
@@ -0,0 +1,20 @@
+# 순차문
+x = 2
+print(x)
+
+x = x + 2
+print(x)
+
+# 조건문
+
+if x < 10:
+    print('smaller')
+if x < 1:
+    print('bigger')
+
+# 반복문
+n = 5
+while n > 0:
+    print(n)
+    n = n-1
+print('blastoff!')
